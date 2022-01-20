67efdff58da528949e1e962aa7f13352586f89e1
surekhakallutla <surekhakallutla@gmail.com>
2022-01-20 18:22:21 +0530

    Welcome to Line Comparision Problem

---
+++ src/com/bridgelabz/oopspart1/LineComparision.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.oopspart1;
+
+public class LineComparision {
+	public static void main(String[] args)	
+	{
+		System.out.println("Welcome to line Comparision Computation Program");
+	}
+
+}
