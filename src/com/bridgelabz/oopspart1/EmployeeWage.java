c1d935db80e876e0381c557a525c7f5b606f0a96
surekhakallutla <surekhakallutla@gmail.com>
2022-01-20 17:11:07 +0530

    Including Part Time Employee and Wage

--- src/com/bridgelabz/oopspart1/EmployeeWage.java
+++ src/com/bridgelabz/oopspart1/EmployeeWage.java
@@ -4,14 +4,15 @@ import java.util.Random;
 
 public class EmployeeWage {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		System.out.println("Welcome to Employee Wage Computation Program");
 		
+		int Wage = 0;
 		int WorkingHour = 0;
 		int Present = 1;
-		int Wage = 0;
+		int Part_Time = 2;
 		int WagePerHour = 20;
-		
 		employee();
 
 		
@@ -20,7 +21,12 @@ public class EmployeeWage {
 			System.out.println("Employee is Present");
 			WorkingHour = 8;
 		}
-		
+
+		else if(employee() == Part_Time)
+		{
+			System.out.println("Employee is Part Timer");
+			WorkingHour = 4;
+		}
 		else
 		{
 			System.out.println("Employee is Absent");
@@ -29,15 +35,15 @@ public class EmployeeWage {
 		
 		Wage = WorkingHour * WagePerHour;
 		System.out.println("Employee wage is "+ Wage);
-		
 	}
 
 	public static double employee()
 	{
-
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-//		System.out.println("Employee check value is "+empCheck);
+	
+		double empCheck = Math.floor(Math.random() * 10) % 3;
+	//	System.out.println("Employee check value is "+empCheck);
 		return empCheck;
-		
+	
 	}
+
 }
