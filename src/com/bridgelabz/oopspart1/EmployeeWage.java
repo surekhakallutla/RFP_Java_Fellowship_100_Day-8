6ed1d47175244b949faaeff5e5fb8d906551a70f
surekhakallutla <surekhakallutla@gmail.com>
2022-01-20 17:47:44 +0530

    Using Switch Case for Calculating Employee Wage problem

--- src/com/bridgelabz/oopspart1/EmployeeWage.java
+++ src/com/bridgelabz/oopspart1/EmployeeWage.java
@@ -10,25 +10,24 @@ public class EmployeeWage {
 		
 		int Wage = 0;
 		int WorkingHour = 0;
-		int Present = 1;
-		int Part_Time = 2;
-		int WagePerHour = 20;
+		final int Present = 1;
+		final int Part_Time = 2;
+		final int WagePerHour = 20;
+		
 		employee();
 
 		
-		if(employee() == Present)
+		switch(employee())
 		{
+		case Present:
 			System.out.println("Employee is Present");
 			WorkingHour = 8;
-		}
-
-		else if(employee() == Part_Time)
-		{
+			break;
+		case Part_Time:
 			System.out.println("Employee is Part Timer");
 			WorkingHour = 4;
-		}
-		else
-		{
+			break;
+		default:
 			System.out.println("Employee is Absent");
 			WorkingHour = 0;
 		}
@@ -37,10 +36,10 @@ public class EmployeeWage {
 		System.out.println("Employee wage is "+ Wage);
 	}
 
-	public static double employee()
+	public static int employee()
 	{
 	
-		double empCheck = Math.floor(Math.random() * 10) % 3;
+		int empCheck =(int) Math.floor(Math.random() * 10) % 3;
 	//	System.out.println("Employee check value is "+empCheck);
 		return empCheck;
 	
