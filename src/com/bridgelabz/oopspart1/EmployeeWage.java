9913b793b29b939b5fb5aeab7e89666d8b77278d
surekhakallutla <surekhakallutla@gmail.com>
2022-01-20 16:53:42 +0530

    Welcome to Employee Wage Computation Program

---
+++ src/com/bridgelabz/oopspart1/EmployeeWage.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.oopspart1;
+
+public class EmployeeWage {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Program");
+		
+
+	}
+
+}
