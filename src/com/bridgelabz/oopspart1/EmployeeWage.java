86258dd3ed4bf36704c9c252609ccfdcf62c4700
surekhakallutla <surekhakallutla@gmail.com>
2022-01-20 17:06:04 +0530

    Calculating Daily Employee Wage

--- src/com/bridgelabz/oopspart1/EmployeeWage.java
+++ src/com/bridgelabz/oopspart1/EmployeeWage.java
@@ -7,19 +7,37 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
 		
-		empcheck();
+		int WorkingHour = 0;
+		int Present = 1;
+		int Wage = 0;
+		int WagePerHour = 20;
+		
+		employee();
 
+		
+		if(employee() == Present)
+		{
+			System.out.println("Employee is Present");
+			WorkingHour = 8;
+		}
+		
+		else
+		{
+			System.out.println("Employee is Absent");
+			WorkingHour = 0;
+		}
+		
+		Wage = WorkingHour * WagePerHour;
+		System.out.println("Employee wage is "+ Wage);
+		
 	}
 
-	public static void empcheck()
+	public static double employee()
 	{
-		double empCheck = Math.floor(Math.random() * 10) % 2;
-		System.out.println("empCheck"+empCheck);
-		int PRESENT = 1;
 
-		if(empCheck == PRESENT)
-			System.out.println("Employee is Present");
-		else
-			System.out.println("Employee is Absent");
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+//		System.out.println("Employee check value is "+empCheck);
+		return empCheck;
+		
 	}
 }
